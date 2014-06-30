a07f6475779afdf6193564725efedb7f6f723124
ThreeSocks <ThreeSocks@localhost>
2014-06-30 18:29:28 +0100

    Initial commit.

---
+++ menu_statics.c
@@ -0,0 +1,123 @@
+/*
+	Statics 1-327 Menu reserved
+*/
+
+// (float) menu_x
+Static_3 = 0.0546875;
+
+// (uint) menu_item_highlighted
+Static_4 = 0;
+
+// (int) menu_count
+Static_5 = 0;
+
+// (bool) menu_hidden
+Static_6 = 0;
+
+// (uint) menu_action_mode (1 = forward, 2= back, 3 = keyboard)
+Static_7 = 0;
+
+// (uint) menu_load_hold_pressed
+Static_8 = 0;
+
+// (uint) menu_level
+Static_9 = 0;
+
+// (char) menu_header
+Static_10 = 0;
+
+// (bool) menu_items_set
+Static_11 = 0;
+
+// (int) instructional_buttons scaleform movie
+Static_12 = 0;
+
+// (bool) scaleform_movie requested
+Static_13 = 0;
+
+// Counters
+//(bool) reset_counter
+Static_14 = 0;
+
+//(uint) press_counter
+Static_15 = 2;
+
+// (uint) hold_counter
+Static_16 = 0;
+
+// (uint) press_counter_timesby
+Static_17 = 1;
+
+// (uint) press_id
+Static_18 = 0;
+
+// (uint) menu_unload_hold_pressed
+Static_19 = 0;
+
+// (float) menu_start_y
+Static_20 = 0.0527
+
+// (float) menu_consts_start_y
+Static_21
+
+// (float) menu_spacing
+Static_22 = 0.03472
+
+// (uint) menu_max
+Static_23
+
+// (uint) menu_consts_max
+Static_24
+
+// (uint) menu_start_scrolling
+Static_25
+
+// (uint) menu_sub_action_mode
+Static_26
+
+// (bool) menu_texture_loaded
+Static_27
+
+// (uint) menu_keyboard_update
+Static_28
+
+// (uint) menu_keyboard_custom_length
+Static_29
+
+// (bool) frontend_items_set
+Static_45
+
+// (int) frontend_count
+Static_46
+
+// (char) frontend_items_button (6)
+Static_47[* 6]
+
+// (char) frontend_items_name (6)
+Static_54[* 6]
+
+// (uint) last_selected (10)
+Static_61[* 10]
+
+// (char) menu_items_name (50)
+Static_72[* 50]
+
+// (uint) menu_items_type (1 = number, 2 = float, 3 = bool, 4 = GXT, 5 = vehicle hash, 6 = keyboard string, 7 = keyboard number, 8 = add number, 9 = gxt add number, 10 = gxt add number2, 11 = gxt number, 12 = keyboard float) (50)
+Static_123[* 50]
+
+// (int) menu_items_extra_val (50)
+Static_174[* 50]
+
+// (bool) menu_items_action (50)
+Static_225[* 50]
+
+// (int) menu_items_num_val (50)
+Static_276[* 50]
+
+// (float) menu_items_float_val (50)
+Static_327[* 50]
+
+// custom_cam
+
+// custom_cam
+Static_378
